ad003f0a6650265539e2a3235453839ebaeb65f5
David Pal <paldavid2030@gmail.com>
2018-05-20 22:23:09 +0200

    Initial Commit
    The pixel art maker was made for the Udacity Nanodegree course. Html and CSS files are from Udacity.Thanks for checking:)

--- designs.js
+++ designs.js
@@ -1,10 +1,39 @@
-// Select color input
-// Select size input
+// Selecting variables
 
-// When size is submitted by the user, call makeGrid()
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWeight');
+const submitButton = document.querySelector('input[type=submit]');
+	//const row = document.createElement('tr');  ---why this doesnt work? is it because theis is later used as parameters of the append method?
+	//const data = document.createElement('td'); ---why this doesnt work? is it because theis is later used as parameters of the append method?
 
-function makeGrid() {
+//The function which creates the grid
 
-// Your code goes here!
+function makeGrid (gridWidth, gridHeight) {
 
-}
+	const table = document.getElementById('pixelCanvas');
+	const colorChoice = document.getElementById('colorPicker');
+
+	table.innerHTML = '';
+
+	for(let w = 0; w < gridWidth.value; w++) {
+		let row = document.createElement('tr');
+		table.append(row);
+		
+		for(let h = 0; h < gridHeight.value; h++) {
+		let data = document.createElement('td');
+		row.append(data);
+
+		data.addEventListener('click', function() {
+			data.style.backgroundColor = colorChoice.value;
+		});
+	}};
+
+};
+
+// Clicking the submit button will prevent the page from reloading and will call the makeGrid function
+
+submitButton.addEventListener('click', function(event) {
+	event.preventDefault();
+	makeGrid (gridWidth, gridHeight);
+});
+
