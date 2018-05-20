8c6f4ac0fd7f54e609932fa987afd5847b960f02
David Pal <paldavid2030@gmail.com>
2018-05-20 22:23:09 +0200

    Initial Commit
    All functionalities are working, in the future I might update the design.

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
-// Select color input
-// Select size input
+// Selecting variables
+	const gridHeight = document.getElementById('inputHeight');
+	const gridWidth = document.getElementById('inputWeight');
+	const table = document.getElementById('pixelCanvas');
+	const submitButton = document.querySelector('input[type=submit]');
+	const colorChoice = document.getElementById('colorPicker');
+// The function which creates the grid
 
-// When size is submitted by the user, call makeGrid()
+function makeGrid (gridWidth, gridHeight) {
 
-function makeGrid() {
+	table.innerHTML = '';
 
-// Your code goes here!
+	for(let w = 0; w < gridWidth.value; w++) {
+		let row = document.createElement('tr');
+		table.append(row);
+		
+		for(let h = 0; h < gridHeight.value; h++) {
+		let data = document.createElement('td');
+		row.append(data);
 
-}
+		data.addEventListener('click', function() {
+			data.style.backgroundColor = colorChoice.value;
+		});
+	}};
+
+};
+
+// Clicking the submit button will prevent the page from reloading and will call the makeGrid function
+
+submitButton.addEventListener('click', function(event) {
+	event.preventDefault();
+	makeGrid (gridWidth, gridHeight);
+});
+
